dd84db02640813989e83d4f85696454aede250c8
James McCarthy <james@brkfst.io>
2022-09-08 14:09:01 -0400

    1.0.13 Add string arrays for usage during run-time and use typeof and Type[number] for types during compile time

--- src/objects/types.ts
+++ src/objects/types.ts
@@ -46,28 +46,34 @@ export type ServiceType = "AUCTION" | "RESERVATION";
 
 export type ReportType = "BASIC" | "AUDIENCE" | "PLAYABLE_MATERIAL" | "CATALOG";
 
-export type DataLevel =
-  | "AUCTION_AD"
-  | "AUCTION_ADGROUP"
-  | "AUCTION_ADVERTISER"
-  | "AUCTION_CAMPAIGN"
-  | "RESERVATION_AD"
-  | "RESERVATION_ADGROUP"
-  | "RESERVATION_ADVERTISER"
-  | "RESERVATION_CAMPAIGN";
+export const DATA_LEVELS = [
+  "AUCTION_AD",
+  "AUCTION_ADGROUP",
+  "AUCTION_ADVERTISER",
+  "AUCTION_CAMPAIGN",
+  "RESERVATION_AD",
+  "RESERVATION_ADGROUP",
+  "RESERVATION_ADVERTISER",
+  "RESERVATION_CAMPAIGN",
+] as const;
+type DataLevelTuple = typeof DATA_LEVELS;
+export type DataLevel = DataLevelTuple[number];
 
 export type OrderType = "ASC" | "DESC";
 
 export type QueryMode = "REGULAR" | "CHUNK";
 
-export type FilterType =
-  | "IN"
-  | "MATCH"
-  | "GREATER_EQUAL"
-  | "GREATER_THAN"
-  | "LOWER_EQUAL"
-  | "LOWER_THAN"
-  | "BETWEEN;";
+export const FILTER_TYPES = [
+  "IN",
+  "MATCH",
+  "GREATER_EQUAL",
+  "GREATER_THAN",
+  "LOWER_EQUAL",
+  "LOWER_THAN",
+  "BETWEEN",
+] as const;
+type FilterTypeTuple = typeof FILTER_TYPES;
+export type FilterType = FilterTypeTuple[number];
 
 export interface Filtering<FieldName = string> {
   field_name: FieldName;
@@ -75,216 +81,256 @@ export interface Filtering<FieldName = string> {
   filter_value: string | string[];
 }
 
-export type Dimension =
-  | "advertiser_id"
-  | "campaign_id"
-  | "adgroup_id"
-  | "ad_id"
-  | "stat_time_day"
-  | "stat_time_hour"
-  | "ac"
-  | "age"
-  | "country_code"
-  | "interest_category"
-  | "gender"
-  | "language"
-  | "placement"
-  | "platform";
+export const DIMENSION = [
+  "advertiser_id",
+  "campaign_id",
+  "adgroup_id",
+  "ad_id",
+  "stat_time_day",
+  "stat_time_hour",
+  "ac",
+  "age",
+  "country_code",
+  "interest_category",
+  "gender",
+  "language",
+  "placement",
+  "platform",
+] as const;
+type DimensionTuple = typeof DIMENSION;
+export type Dimension = DimensionTuple[number];
 
-export type BasicFilterField =
-  | "campaign_ids"
-  | "campaign_type"
-  | "campaign_status"
-  | "adgroup_ids"
-  | "adgroup_status"
-  | "ad_ids"
-  | "ad_status"
-  | "promotion_type"
-  | "dpa_target_audience_type"
-  | "create_time";
+export const BASIC_FILTER_FIELDS = [
+  "campaign_ids",
+  "campaign_type",
+  "campaign_status",
+  "adgroup_ids",
+  "adgroup_status",
+  "ad_ids",
+  "ad_status",
+  "promotion_type",
+  "dpa_target_audience_type",
+  "create_time",
+] as const;
+type BasicFilterFieldTuple = typeof BASIC_FILTER_FIELDS;
+export type BasicFilterField = BasicFilterFieldTuple[number];
 
-export type BasicDimension =
-  | "advertiser_id"
-  | "campaign_id"
-  | "adgroup_id"
-  | "ad_id"
-  | "stat_time_day"
-  | "stat_time_hour"
-  | "country_code";
+export const BASIC_DIMENSIONS = [
+  "advertiser_id",
+  "campaign_id",
+  "adgroup_id",
+  "ad_id",
+  "stat_time_day",
+  "stat_time_hour",
+  "country_code",
+] as const;
+type BasicDimensionTuple = typeof BASIC_DIMENSIONS;
+export type BasicDimension = BasicDimensionTuple[number];
 
-export type AttributeMetric =
-  | "campaign_name"
-  | "objective_type"
-  | "campaign_id"
-  | "adgroup_name"
-  | "placement_type"
-  | "adgroup_id"
-  | "ad_name"
-  | "ad_text"
-  | "tt_app_id"
-  | "tt_app_name"
-  | "mobile_app_id"
-  | "promotion_type"
-  | "dpa_target_audience_type";
+export const ATTRIBUTE_METRICS = [
+  "campaign_name",
+  "objective_type",
+  "campaign_id",
+  "adgroup_name",
+  "placement_type",
+  "adgroup_id",
+  "ad_name",
+  "ad_text",
+  "tt_app_id",
+  "tt_app_name",
+  "mobile_app_id",
+  "promotion_type",
+  "dpa_target_audience_type",
+];
+type AttributeMetricTuple = typeof ATTRIBUTE_METRICS;
+export type AttributeMetric = AttributeMetricTuple[number];
 
-export type BasicDataMetric =
-  | "spend"
-  | "cash_spend"
-  | "voucher_spend"
-  | "cpc"
-  | "cpm"
-  | "impressions"
-  | "clicks"
-  | "ctr"
-  | "reach"
-  | "cost_per_1000_reached"
-  | "conversion"
-  | "cost_per_conversion"
-  | "conversion_rate"
-  | "real_time_conversion"
-  | "real_time_cost_per_conversion"
-  | "real_time_conversion_rate"
-  | "result"
-  | "cost_per_result"
-  | "result_rate"
-  | "real_time_result"
-  | "real_time_cost_per_result"
-  | "real_time_result_rate"
-  | "secondary_goal_result"
-  | "cost_per_secondary_goal_result"
-  | "secondary_goal_result_rate"
-  | "frequency"
-  | "currency";
+export const BASIC_DATA_METRICS = [
+  "spend",
+  "cash_spend",
+  "voucher_spend",
+  "cpc",
+  "cpm",
+  "impressions",
+  "clicks",
+  "ctr",
+  "reach",
+  "cost_per_1000_reached",
+  "conversion",
+  "cost_per_conversion",
+  "conversion_rate",
+  "real_time_conversion",
+  "real_time_cost_per_conversion",
+  "real_time_conversion_rate",
+  "result",
+  "cost_per_result",
+  "result_rate",
+  "real_time_result",
+  "real_time_cost_per_result",
+  "real_time_result_rate",
+  "secondary_goal_result",
+  "cost_per_secondary_goal_result",
+  "secondary_goal_result_rate",
+  "frequency",
+  "currency",
+] as const;
+type BasicDataMetricTuple = typeof BASIC_DATA_METRICS;
+export type BasicDataMetric = BasicDataMetricTuple[number];
 
-export type VideoPlayMetric =
-  | "video_play_actions"
-  | "video_watched_2s"
-  | "video_watched_6s"
-  | "average_video_play"
-  | "average_video_play_per_user"
-  | "video_views_p25"
-  | "video_views_p50"
-  | "video_views_p75"
-  | "video_views_p100"
-  | "engaged_view";
+export const VIDEO_PLAY_METRICS = [
+  "video_play_actions",
+  "video_watched_2s",
+  "video_watched_6s",
+  "average_video_play",
+  "average_video_play_per_user",
+  "video_views_p25",
+  "video_views_p50",
+  "video_views_p75",
+  "video_views_p100",
+  "engaged_view",
+] as const;
+type VideoPlayMetricTuple = typeof VIDEO_PLAY_METRICS;
+export type VideoPlayMetric = VideoPlayMetricTuple[number];
 
-export type EngagementMetric =
-  | "profile_visits"
-  | "profile_visits_rate"
-  | "likes"
-  | "comments"
-  | "shares"
-  | "follows"
-  | "clicks_on_music_disc";
+export const ENGAGEMENT_METRICS = [
+  "profile_visits",
+  "profile_visits_rate",
+  "likes",
+  "comments",
+  "shares",
+  "follows",
+  "clicks_on_music_disc",
+];
+type EngagementMetricTuple = typeof ENGAGEMENT_METRICS;
+export type EngagementMetric = EngagementMetricTuple[number];
 
-export type AttributionMetric =
-  | "vta_app_install"
-  | "vta_conversion"
-  | "cost_per_vta_conversion"
-  | "vta_registration"
-  | "cost_per_vta_registration"
-  | "vta_purchase"
-  | "cost_per_vta_purchase"
-  | "cta_app_install"
-  | "cta_conversion"
-  | "cost_per_cta_conversion"
-  | "cta_registration"
-  | "cost_per_cta_registration"
-  | "cta_purchase"
-  | "cost_per_cta_purchase";
+export const ATTRIBUTION_METRICS = [
+  "vta_app_install",
+  "vta_conversion",
+  "cost_per_vta_conversion",
+  "vta_registration",
+  "cost_per_vta_registration",
+  "vta_purchase",
+  "cost_per_vta_purchase",
+  "cta_app_install",
+  "cta_conversion",
+  "cost_per_cta_conversion",
+  "cta_registration",
+  "cost_per_cta_registration",
+  "cta_purchase",
+  "cost_per_cta_purchase",
+] as const;
+type AttributionMetricTuple = typeof ATTRIBUTION_METRICS;
+export type AttributionMetric = AttributionMetricTuple[number];
 
-export type PageEventMetric =
-  | "complete_payment_roas"
-  | "complete_payment"
-  | "cost_per_complete_payment"
-  | "complete_payment_rate"
-  | "value_per_complete_payment"
-  | "total_complete_payment_rate"
-  | "page_browse_view"
-  | "cost_per_page_browse_view"
-  | "page_browse_view_rate"
-  | "total_page_browse_view_value"
-  | "value_per_page_browse_view"
-  | "button_click"
-  | "cost_per_button_click"
-  | "button_click_rate"
-  | "value_per_button_click"
-  | "total_button_click_value"
-  | "online_consult"
-  | "cost_per_online_consult"
-  | "online_consult_rate"
-  | "value_per_online_consult"
-  | "total_online_consult_value"
-  | "user_registration"
-  | "cost_per_user_registration"
-  | "user_registration_rate"
-  | "value_per_user_registration"
-  | "total_user_registration_value"
-  | "product_details_page_browse"
-  | "cost_per_product_details_page_browse"
-  | "product_details_page_browse_rate"
-  | "value_per_product_details_page_browse"
-  | "total_product_details_page_browse_value"
-  | "web_event_add_to_cart"
-  | "cost_per_web_event_add_to_cart"
-  | "web_event_add_to_cart_rate"
-  | "value_per_web_event_add_to_cart"
-  | "total_web_event_add_to_cart_value"
-  | "on_web_order"
-  | "cost_per_on_web_order"
-  | "on_web_order_rate"
-  | "value_per_on_web_order"
-  | "total_on_web_order_value"
-  | "initiate_checkout"
-  | "cost_per_initiate_checkout"
-  | "initiate_checkout_rate"
-  | "value_per_initiate_checkout"
-  | "total_initiate_checkout_value"
-  | "add_billing"
-  | "cost_per_add_billing"
-  | "add_billing_rate"
-  | "value_per_add_billing"
-  | "total_add_billing_value"
-  | "page_event_search"
-  | "cost_per_page_event_search"
-  | "page_event_search_rate"
-  | "value_per_page_event_search"
-  | "total_page_event_search_value"
-  | "form"
-  | "cost_per_form"
-  | "form_rate"
-  | "value_per_form"
-  | "total_form_value"
-  | "download_start"
-  | "cost_per_download_start"
-  | "download_start_rate"
-  | "value_per_download_start"
-  | "total_download_start_value"
-  | "on_web_add_to_wishlist"
-  | "cost_per_on_web_add_to_wishlist"
-  | "on_web_add_to_wishlist_per_click"
-  | "value_per_on_web_add_to_wishlist"
-  | "total_on_web_add_to_wishlist_value"
-  | "on_web_subscribe"
-  | "cost_per_on_web_subscribe"
-  | "on_web_subscribe_per_click"
-  | "value_per_on_web_subscribe"
-  | "total_on_web_subscribe_value";
+export const PAGE_EVENT_METRICS = [
+  "complete_payment_roas",
+  "complete_payment",
+  "cost_per_complete_payment",
+  "complete_payment_rate",
+  "value_per_complete_payment",
+  "total_complete_payment_rate",
+  "page_browse_view",
+  "cost_per_page_browse_view",
+  "page_browse_view_rate",
+  "total_page_browse_view_value",
+  "value_per_page_browse_view",
+  "button_click",
+  "cost_per_button_click",
+  "button_click_rate",
+  "value_per_button_click",
+  "total_button_click_value",
+  "online_consult",
+  "cost_per_online_consult",
+  "online_consult_rate",
+  "value_per_online_consult",
+  "total_online_consult_value",
+  "user_registration",
+  "cost_per_user_registration",
+  "user_registration_rate",
+  "value_per_user_registration",
+  "total_user_registration_value",
+  "product_details_page_browse",
+  "cost_per_product_details_page_browse",
+  "product_details_page_browse_rate",
+  "value_per_product_details_page_browse",
+  "total_product_details_page_browse_value",
+  "web_event_add_to_cart",
+  "cost_per_web_event_add_to_cart",
+  "web_event_add_to_cart_rate",
+  "value_per_web_event_add_to_cart",
+  "total_web_event_add_to_cart_value",
+  "on_web_order",
+  "cost_per_on_web_order",
+  "on_web_order_rate",
+  "value_per_on_web_order",
+  "total_on_web_order_value",
+  "initiate_checkout",
+  "cost_per_initiate_checkout",
+  "initiate_checkout_rate",
+  "value_per_initiate_checkout",
+  "total_initiate_checkout_value",
+  "add_billing",
+  "cost_per_add_billing",
+  "add_billing_rate",
+  "value_per_add_billing",
+  "total_add_billing_value",
+  "page_event_search",
+  "cost_per_page_event_search",
+  "page_event_search_rate",
+  "value_per_page_event_search",
+  "total_page_event_search_value",
+  "form",
+  "cost_per_form",
+  "form_rate",
+  "value_per_form",
+  "total_form_value",
+  "download_start",
+  "cost_per_download_start",
+  "download_start_rate",
+  "value_per_download_start",
+  "total_download_start_value",
+  "on_web_add_to_wishlist",
+  "cost_per_on_web_add_to_wishlist",
+  "on_web_add_to_wishlist_per_click",
+  "value_per_on_web_add_to_wishlist",
+  "total_on_web_add_to_wishlist_value",
+  "on_web_subscribe",
+  "cost_per_on_web_subscribe",
+  "on_web_subscribe_per_click",
+  "value_per_on_web_subscribe",
+  "total_on_web_subscribe_value",
+];
+type PageEventMetricTuple = typeof PAGE_EVENT_METRICS;
+export type PageEventMetric = PageEventMetricTuple[number];
 
-export type OnsiteEventMetric =
-  | "onsite_shopping_roas"
-  | "onsite_shopping"
-  | "onsite_initiate_checkout_count"
-  | "onsite_on_web_detail"
-  | "onsite_add_to_wishlist"
-  | "onsite_add_billing"
-  | "onsite_on_web_cart"
-  | "onsite_form"
-  | "onsite_download_start"
-  | "ix_page_view_count"
-  | "ix_button_click_count"
-  | "ix_product_click_count";
+export const ONSITE_EVENT_METRICS = [
+  "onsite_shopping_roas",
+  "onsite_shopping",
+  "onsite_initiate_checkout_count",
+  "onsite_on_web_detail",
+  "onsite_add_to_wishlist",
+  "onsite_add_billing",
+  "onsite_on_web_cart",
+  "onsite_form",
+  "onsite_download_start",
+  "ix_page_view_count",
+  "ix_button_click_count",
+  "ix_product_click_count",
+] as const;
+type OnsiteEventMetricTuple = typeof ONSITE_EVENT_METRICS;
+export type OnsiteEventMetric = OnsiteEventMetricTuple[number];
 
+export const BASIC_METRICS = Object.freeze(
+  ATTRIBUTE_METRICS.concat(
+    BASIC_DATA_METRICS,
+    VIDEO_PLAY_METRICS,
+    ENGAGEMENT_METRICS,
+    ATTRIBUTE_METRICS,
+    PAGE_EVENT_METRICS,
+    ONSITE_EVENT_METRICS
+  )
+);
 export type BasicMetric =
   | AttributeMetric
   | BasicDataMetric
