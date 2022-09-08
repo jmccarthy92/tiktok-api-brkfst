2139d8f88d9936380347f253d9eb2448fbd8cc46
James McCarthy <james@brkfst.io>
2022-09-08 16:55:34 -0400

    1.0.14 ADd export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as TikTokApi } from "./api";
 export { default as TikTokAuth } from "./objects/oauth";
 export { default as TikTokVideo } from "./objects/video";
+export { default as TikTokReport } from "./objects/report";
 export * from "./objects/types";
