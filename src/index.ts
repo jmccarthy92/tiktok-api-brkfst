8c6163755b348aa2ba2698dd31a036eedd5df26a
James McCarthy <james@brkfst.io>
2022-09-14 11:10:22 -0400

    1.0.21 Update barrel export

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export { default as TikTokReport } from "./objects/report";
 export { default as TikTokCampaign } from "./objects/campaign";
 export { default as TikTokAdGroup } from "./objects/adgroup";
 export { default as TikTokAd } from "./objects/ad";
+export { default as TikTokAdAccount } from "./objects/adaccount";
 export * from "./objects/types";
